47252ef7f9fa6f0ba1e6fae43b701234c710f813
OlegGorod <108138364+OlegGorod@users.noreply.github.com>
2023-02-11 23:22:13 +0200

    Add files via upload

---
+++ movieSlider/script.js
@@ -0,0 +1,148 @@
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = 'https://image.tmdb.org/t/p/w500';
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const main = document.querySelector('main');
+const form = document.querySelector('form');
+const sortBtn = document.querySelector('.sorting');
+const wrapper = document.querySelectorAll('.wrapper');
+let dataResponse;
+let renderArr = [];
+
+getMovies(APIURL);
+async function getMovies(url) {
+  const response = await fetch(url);
+  dataResponse = await response.json();
+  arrayOfMovies = dataResponse.results;
+  console.log(arrayOfMovies)
+  
+  showMovies(arrayOfMovies);
+}
+
+showMovies = (data) => {
+  renderArr = [];
+  main.innerHTML = '';
+  renderSlickBtn()
+
+  const next = document.querySelector('.slick_next');
+  const prev = document.querySelector('.slick_prev');
+  let slideIndex = 0;
+
+  let fetchedMovies = data;
+  console.log(fetchedMovies)
+  let chunkQuantity = 4;
+  for (let i = 0; i < fetchedMovies.length; i += chunkQuantity) {
+    renderArr.push(fetchedMovies.slice(i, chunkQuantity + i))
+  }
+  console.log(renderArr)
+  renderArr[slideIndex].forEach(element => {
+    const { poster_path, title, vote_average } = element;
+
+    if (poster_path) {
+
+      const movie = document.createElement('div');
+
+
+      movie.classList.add('movie');
+      movie.style.display = 'block'
+      main.appendChild(movie);
+      movie.innerHTML = `
+    <div class="wrapper">
+        <img src="${IMGPATH + poster_path}" alt="">
+        <div class="${colorVote(vote_average)} vote_average">${vote_average}</div>
+    </div>
+    <div class="movie_info">${title}</div>
+    `
+    }
+  });
+
+
+
+
+  next.addEventListener('click', () => {
+    slideIndex += 1;
+    let film = document.querySelectorAll('.movie')
+    film.forEach(item => item.style.display = 'none')
+    console.log(slideIndex)
+    if (slideIndex > renderArr.length - 1) {
+      slideIndex = 0
+    }
+    renderArr[slideIndex].forEach(element => {
+      const { poster_path, title, vote_average } = element;
+      if (poster_path) {
+        const movie = document.createElement('div');
+        const offer = document.createElement('div');
+        movie.classList.add('movie');
+        offer.classList.add('offer');
+        movie.style.display = 'block'
+        main.appendChild(movie);
+        movie.innerHTML = `
+      <div class="wrapper">
+          <img src="${IMGPATH + poster_path}" alt="">
+          <div class="${colorVote(vote_average)} vote_average">${vote_average}</div>
+      </div>
+      <div class="movie_info">${title}</div>
+      `
+      }
+    });
+  });
+
+  prev.addEventListener('click', () => {
+    slideIndex--;
+    let film = document.querySelectorAll('.movie')
+    film.forEach(item => item.style.display = 'none')
+    if (slideIndex < 0) {
+      slideIndex = renderArr.length - 1;
+    }
+    console.log(slideIndex)
+    renderArr[slideIndex].forEach(element => {
+      const { poster_path, title, vote_average } = element;
+      if (poster_path) {
+        const movie = document.createElement('div');
+        movie.classList.add('movie');
+        movie.style.display = 'block'
+        main.appendChild(movie);
+        movie.innerHTML = `
+      <div class="wrapper">
+          <img src="${IMGPATH + poster_path}" alt="">
+          <div class="${colorVote(vote_average)} vote_average">${vote_average}</div>
+      </div>
+      <div class="movie_info">${title}</div>
+      `
+      }
+    });
+  });
+
+}
+
+function renderSlickBtn() {
+  const back = document.createElement('div')
+  const forward = document.createElement('div')
+  back.classList.add('slick_prev');
+  back.innerHTML = '<i class="fa-solid fa-angle-left"></i>';
+  forward.classList.add('slick_next');
+  forward.innerHTML = '<i class="fa-solid fa-angle-right"></i>';
+  main.append(forward);
+  main.prepend(back)
+
+}
+
+function colorVote(info) {
+  if (info >= 8) {
+    return 'green'
+  } if (info > 6) {
+    return 'orange'
+  } else return 'red'
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const value = search.value;
+  getMovies(SEARCHAPI + value);
+});
+
+sortBtn.addEventListener('click', async () => {
+  renderArr = arrayOfMovies.sort((a, b) => b.vote_average - a.vote_average);
+  console.log(renderArr)
+  showMovies(renderArr);
+});
